ca0a056faf665fc31153fca15b8d164b9698e63a
Prayrit <sriprayrit@gmail.com>
2022-06-02 15:46:02 +0530

    change1

---
+++ src/Components/Canvas.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { fabric } from "fabric";
+
+
+class Canvas extends React.Component {
+    render(){
+        return(
+            <>
+            <h1>this is where the fabric will be displayed</h1>,
+            </>
+        )
+    }
+}
+export default Canvas;
